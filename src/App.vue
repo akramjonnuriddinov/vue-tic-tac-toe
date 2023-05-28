4c4d8a0965ab680264485c1e23657f01738bf111
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2023-05-29 02:27:35 +0500

    git init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="font-medium container shadow rounded p-5 mx-auto max-w-sm">
+    <h1 class="text-2xl">
+      Next player: <span class="uppercase">{{ 'x' }}</span>
+    </h1>
+
+    <ul class="py-5 w-[200px] flex flex-wrap">
+      <li class="" v-for="(click_value, index) in click_values" :key="index">
+        <button
+          v-for="(j, idx) in click_value"
+          :key="idx"
+          @click="clickButton(index, idx)"
+          class="border w-10 flex items-center justify-center text-2xl h-10 py-4 px-5 uppercase hover:bg-gray-100 active:bg-gray-200"
+        >
+          {{ j }}
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      click_values: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+      ],
+      count: 0
+    }
+  },
+  methods: {
+    clickButton(i, j) {
+      if (this.count % 2 == 0) {
+        this.click_values[i][j] = 'x'
+        this.count++
+      } else {
+        this.click_values[i][j] = '0'
+        this.count++
+      }
+    }
+  },
+  mounted() {
+    console.log(this.click_values)
+  }
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
