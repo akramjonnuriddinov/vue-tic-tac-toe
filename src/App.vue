791b33b20ee1e9a1ecf3fc44b43a7eec93484214
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2023-06-07 15:13:07 +0500

    Add TicTacToe.vue

--- src/App.vue
+++ src/App.vue
@@ -1,124 +1,13 @@
 <template>
-  <div class="container max-w-sm p-5 mx-auto text-xl">
-    <h1>
-      Next player: <span class="uppercase">{{ active }}</span>
-    </h1>
-    <div class="flex items-start gap-10">
-      <div class="my-5" style="width: 126px; display: flex; flex-wrap: wrap">
-        <button
-          v-for="(box, index) in boxes"
-          :key="index"
-          @click="clickButton(index)"
-          :disabled="box.length"
-          :class="{
-            'bg-red-200': win,
-            'cursor-not-allowed': win
-          }"
-          class="flex items-center justify-center w-10 h-10 px-5 py-4 uppercase border hover:bg-gray-100 active:bg-gray-200 disabled:bg-red-50"
-        >
-          {{ box }}
-        </button>
-      </div>
-      <div>
-        <button
-          class="px-3 py-1 ml-auto text-lg text-white bg-gray-600 rounded"
-          @click="restart"
-        >
-          Restart
-        </button>
-        <div class="flex flex-wrap gap-2 w-[160px] py-1 text-sm">
-          <button
-            v-for="(_, index) in count"
-            :key="index"
-            @click="route(index)"
-            class="font-normal underline"
-          >
-            shag#{{ index + 1 }}
-          </button>
-        </div>
-      </div>
-    </div>
-    <div>
-      <h2 class="mb-2">
-        Result:
-        <span class="text-green-500" v-if="win">{{ win }}</span>
-        <span class="text-green-500" v-else>None</span> player won
-      </h2>
-    </div>
-  </div>
+  <tic-tac-toe />
 </template>
 
 <script>
+import TicTacToe from '@/components/TicTacToe'
 export default {
   name: 'App',
-  data() {
-    return {
-      boxes: ['', '', '', '', '', '', '', '', ''],
-      combinations: [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-      ],
-      flag: true,
-      steps: [],
-      count: 0
-    }
-  },
-  // watch: {
-  //   boxes: {
-  //     deep: true,
-  //     handler(value) {
-  //       this.steps.push([...value])
-  //     }
-  //   }
-  // },
-  computed: {
-    active() {
-      return this.flag ? 'X' : 'O'
-    },
-    win() {
-      let isWin = false
-      let winnedPlayer = ''
-      for (let i = 0; i < this.combinations.length; i++) {
-        const combination = this.combinations[i]
-        if (
-          this.boxes[combination[0]] === this.boxes[combination[1]] &&
-          this.boxes[combination[0]] === this.boxes[combination[2]]
-        ) {
-          isWin = true
-          winnedPlayer = this.boxes[combination[0]]
-          break
-        }
-      }
-      return isWin ? winnedPlayer : false
-    }
-  },
-  methods: {
-    updateCounter() {
-      this.counter++
-    },
-    clickButton(index) {
-      this.boxes[index] = this.active
-      this.steps.push([...this.boxes])
-      this.flag = !this.flag
-      this.count++
-    },
-    restart() {
-      this.boxes = ['', '', '', '', '', '', '', '', '']
-      this.steps = []
-      this.flag = true
-      this.count = 0
-    },
-    route(index) {
-      this.boxes = this.steps[index]
-      this.steps = this.steps.filter((step) => this.steps.indexOf(step) < index)
-      this.count = index + 1
-    }
+  components: {
+    TicTacToe
   }
 }
 </script>
---
+++ src/components/TicTacToe.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="container max-w-md px-5 py-5 mx-auto text-xl">
+    <div class="flex gap-10">
+      <label for="x3">
+        3x
+        <input value="3" v-model="game_size" id="x3" type="radio" name="game" />
+      </label>
+      <label for="x4">
+        4x
+        <input value="4" v-model="game_size" id="x4" type="radio" name="game" />
+      </label>
+      <div>Game size: {{ game_size }}</div>
+    </div>
+
+    <p>Next player: {{ isActive }}</p>
+
+    <div class="flex items-start justify-between">
+      <ul
+        class="flex flex-wrap border"
+        :class="{
+          'max-w-[122px]': game_size == 3,
+          'max-w-[162px]': game_size == 4
+        }"
+      >
+        <li v-for="(box, index) in boxes[game_size]" :key="index">
+          <button
+            :disabled="box.length || disabledBtn"
+            @click="click(index)"
+            :class="{ 'bg-red-50': win }"
+            class="flex items-center justify-center w-10 h-10 text-xl border disabled:bg-gray-100"
+          >
+            {{ box }}
+          </button>
+        </li>
+      </ul>
+      <ul class="flex flex-wrap gap-2 text-base max-w-[100px]">
+        <li v-for="(_, index) in steps" :key="index">
+          <button @click="router(index)" class="underline">
+            #{{ index + 1 }}
+          </button>
+        </li>
+      </ul>
+    </div>
+
+    <div class="flex gap-5 pt-5">
+      <div>Win: {{ win }}</div>
+      <span>|</span>
+      <button @click="restart">Restart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TicTacToe',
+  data() {
+    return {
+      boxes: {
+        3: ['', '', '', '', '', '', '', '', ''],
+        4: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+      },
+      count: 0,
+      flag: true,
+      combinations: {
+        3: [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8]
+        ],
+        4: [
+          [0, 1, 2, 3],
+          [4, 5, 6, 7],
+          [8, 9, 10, 11],
+          [12, 13, 14, 15],
+
+          [0, 5, 10, 15],
+          [3, 6, 9, 12],
+
+          [0, 4, 8, 12],
+          [1, 5, 9, 13],
+          [2, 6, 10, 14],
+          [3, 7, 11, 15]
+        ]
+      },
+      steps: [],
+      game_size: 3
+    }
+  },
+  watch: {
+    game_size() {
+      this.steps = []
+    }
+  },
+  computed: {
+    isActive() {
+      return this.flag ? 'X' : 'O'
+    },
+    win() {
+      let isWin = false
+      let winnedPlayer = false
+      for (let i = 0; i < this.combinations[this.game_size].length; i++) {
+        const combination = this.combinations[this.game_size][i]
+        let checkWin = true
+        for (let j = 0; j < combination.length; j++) {
+          if (
+            this.boxes[this.game_size][combination[0]] !==
+            this.boxes[this.game_size][combination[j]]
+          ) {
+            checkWin = false
+          }
+        }
+        if (checkWin && this.boxes[this.game_size][combination[0]] !== '') {
+          isWin = true
+          winnedPlayer = this.boxes[this.game_size][combination[0]]
+          break
+        }
+      }
+      return isWin ? winnedPlayer : false
+    },
+    disabledBtn() {
+      if (this.win == '' || this.win == false) {
+        return false
+      }
+      return true
+    }
+  },
+  methods: {
+    click(index) {
+      this.boxes[this.game_size][index] = this.isActive
+      this.flag = !this.flag
+      this.steps.push([...this.boxes[this.game_size]])
+    },
+    router(index) {
+      this.boxes[this.game_size] = this.steps[index]
+      this.steps = this.steps.filter((step) => this.steps.indexOf(step) < index)
+    },
+    restart() {
+      if (this.game_size == 3) {
+        this.boxes[this.game_size] = ['', '', '', '', '', '', '', '', '']
+      } else {
+        this.boxes[this.game_size] = [
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '',
+          ''
+        ]
+      }
+      this.steps = []
+    }
+  }
+}
+</script>
